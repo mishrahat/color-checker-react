de83003afd3859bda7c177b1ab99e61d52fdf1e8
Mishary Rafique <misharyrafique01@gmail.com>
2023-01-11 04:02:33 +0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [color, setColor] = useState("black");
+  const colors = ["blue", "black", "orange", "skyblue"];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Color Checker:
+      <div
+        style={{
+          backgroundColor: color || "black",
+          width: "100px",
+          height: "100px",
+        }}
+      ></div>
+      <input
+        value={color}
+        placeholder="Change Color: e.g. Black"
+        onChange={(e) => setColor(e.target.value)}
+      />
+      <p> or choose from here: </p>
+      {colors?.map((colorname, index) => (
+        <button onClick={() => setColor(colorname)}>{colorname}</button>
+      ))}
     </div>
   );
 }
